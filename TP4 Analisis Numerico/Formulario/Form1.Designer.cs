709e1071d7f364cefadf6d882f2bf2cc5cf42b3d
Diego <diegocarpbressan@gmail.com>
2019-10-30 15:36:21 -0300

    Creeria que anda todo pefeto

--- TP4 Analisis Numerico/Formulario/Form1.Designer.cs
+++ TP4 Analisis Numerico/Formulario/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TrapezoideSimple = new System.Windows.Forms.Button();
+            this.TrapezoideMultiple = new System.Windows.Forms.Button();
+            this.SimpsonSimpleUnTercio = new System.Windows.Forms.Button();
+            this.SimpsonMultipleUnTercio = new System.Windows.Forms.Button();
+            this.SimpsonTresOctavos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Analisis Numerico";
+            // 
+            // TrapezoideSimple
+            // 
+            this.TrapezoideSimple.Location = new System.Drawing.Point(103, 129);
+            this.TrapezoideSimple.Name = "TrapezoideSimple";
+            this.TrapezoideSimple.Size = new System.Drawing.Size(136, 32);
+            this.TrapezoideSimple.TabIndex = 1;
+            this.TrapezoideSimple.Text = "Trapezoide Simple";
+            this.TrapezoideSimple.UseVisualStyleBackColor = true;
+            // 
+            // TrapezoideMultiple
+            // 
+            this.TrapezoideMultiple.Location = new System.Drawing.Point(103, 167);
+            this.TrapezoideMultiple.Name = "TrapezoideMultiple";
+            this.TrapezoideMultiple.Size = new System.Drawing.Size(136, 32);
+            this.TrapezoideMultiple.TabIndex = 2;
+            this.TrapezoideMultiple.Text = "Trapezoide Multiple";
+            this.TrapezoideMultiple.UseVisualStyleBackColor = true;
+            // 
+            // SimpsonSimpleUnTercio
+            // 
+            this.SimpsonSimpleUnTercio.Location = new System.Drawing.Point(103, 205);
+            this.SimpsonSimpleUnTercio.Name = "SimpsonSimpleUnTercio";
+            this.SimpsonSimpleUnTercio.Size = new System.Drawing.Size(136, 32);
+            this.SimpsonSimpleUnTercio.TabIndex = 3;
+            this.SimpsonSimpleUnTercio.Text = "Simpson 1/3 Simple";
+            this.SimpsonSimpleUnTercio.UseVisualStyleBackColor = true;
+            // 
+            // SimpsonMultipleUnTercio
+            // 
+            this.SimpsonMultipleUnTercio.Location = new System.Drawing.Point(103, 243);
+            this.SimpsonMultipleUnTercio.Name = "SimpsonMultipleUnTercio";
+            this.SimpsonMultipleUnTercio.Size = new System.Drawing.Size(136, 32);
+            this.SimpsonMultipleUnTercio.TabIndex = 4;
+            this.SimpsonMultipleUnTercio.Text = "Simpson 1/3 Multiple";
+            this.SimpsonMultipleUnTercio.UseVisualStyleBackColor = true;
+            // 
+            // SimpsonTresOctavos
+            // 
+            this.SimpsonTresOctavos.Location = new System.Drawing.Point(103, 281);
+            this.SimpsonTresOctavos.Name = "SimpsonTresOctavos";
+            this.SimpsonTresOctavos.Size = new System.Drawing.Size(136, 32);
+            this.SimpsonTresOctavos.TabIndex = 5;
+            this.SimpsonTresOctavos.Text = "Simpson 3/8";
+            this.SimpsonTresOctavos.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(363, 450);
+            this.Controls.Add(this.SimpsonTresOctavos);
+            this.Controls.Add(this.SimpsonMultipleUnTercio);
+            this.Controls.Add(this.SimpsonSimpleUnTercio);
+            this.Controls.Add(this.TrapezoideMultiple);
+            this.Controls.Add(this.TrapezoideSimple);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button TrapezoideSimple;
+        private System.Windows.Forms.Button TrapezoideMultiple;
+        private System.Windows.Forms.Button SimpsonSimpleUnTercio;
+        private System.Windows.Forms.Button SimpsonMultipleUnTercio;
+        private System.Windows.Forms.Button SimpsonTresOctavos;
     }
 }
 
--- TP4 Analisis Numerico/Logica/Principal.cs
+++ TP4 Analisis Numerico/Logica/Principal.cs
@@ -8,6 +8,122 @@ namespace Logica
 {
     public class Principal
     {
+        public double ObtenerFuncion(double Limite)
+        {
+            // Calcula el f(limite) pedido
+            double resultadoo = new double();
+            double aux1 = 0;
+            double aux2 = 0;
 
+            //Actividad 2 A
+            aux1 = 1 / (Limite + 0.5);
+            aux2 = (1 / 4) * Math.Pow(Limite, 2);
+            resultadoo = aux1 + aux2;
+
+            //Actividad 2 B
+            resultadoo = 1 + Math.Pow(Limite, 2);
+            resultadoo = Math.Log(resultadoo);
+
+            //Actividad 2 C
+            resultadoo = Math.Pow(Math.E, Limite) * (1 + (0.5 * Math.Pow(Limite, 2)));
+
+            //Actividad 3
+            double Gfuncion = (-3 / 4) * Math.Pow(Limite, 2);
+            Gfuncion = Gfuncion - Limite + 4;
+            aux1 = (1 / 16) * Math.Pow(Limite, 4);
+            aux2 = (1 / 4) * Math.Pow(Limite, 3);
+            double Ffuncion = aux1 - aux2;
+            resultadoo = Gfuncion - Ffuncion;
+
+            return resultadoo;
+        }
+
+        //Trapezoide Simple
+        public double MetodoTrapezoideSimple(Datos dato)
+        {
+            double Area = 0;
+
+            Area = ObtenerFuncion(dato.ValorXA) + ObtenerFuncion(dato.ValorXB);
+            Area = Area * (dato.ValorXB * dato.ValorXA);
+            Area = Area / 2;
+
+            return Area;
+        }
+
+        //TrapezoideMultiple
+        public double MetodoTrapezoideMultiple(Datos dato, int Nintervalos)
+        {
+            double Area = 0;
+            double h = dato.ValorXB - dato.ValorXA;
+            double aux = (dato.ValorXB - dato.ValorXA) / Nintervalos;
+            double intervalo = dato.ValorXA + aux;
+
+            for (int i = 1; i < Nintervalos; i++)
+            {
+                Area += ObtenerFuncion(intervalo);
+                intervalo += aux;
+            }
+            Area = (Area * 2) + ObtenerFuncion(dato.ValorXA) + ObtenerFuncion(dato.ValorXB);
+            Area = Area * (h / 2);
+
+            return Area;
+        }
+
+        //Simpson 1/3 Simple
+        public double MetodoSimpsonSimpleUnTercio(Datos dato)
+        {
+            double Area = 0;
+            double x1 = (dato.ValorXA + dato.ValorXB) / 2;
+            double h = (dato.ValorXB - dato.ValorXA) / 2;
+
+            Area = ObtenerFuncion(dato.ValorXB) + (ObtenerFuncion(x1) * 4) + ObtenerFuncion(dato.ValorXA);
+
+            return Area;
+        }
+
+        //Simpson 1/3 Multiple
+        public double MetodoSimpsonMultipleUnTercio(Datos dato, int Nintervalos)
+        {
+            double Area = 0;
+            double h = (dato.ValorXB - dato.ValorXA) / Nintervalos;
+            double intervalo = dato.ValorXA + h;
+            bool ban = false;
+            double aux1 = 0;
+            double aux2 = 0;
+
+            for (int i = 1; i < Nintervalos; i++)
+            {
+                if (ban == false)
+                {
+                    aux1 += ObtenerFuncion(intervalo);
+                    ban = true;
+                }
+                else
+                {
+                    aux2 += ObtenerFuncion(intervalo);
+                    ban = true;
+                }
+                intervalo += h;
+            }
+            Area = ObtenerFuncion(dato.ValorXA) + (4 * aux1) + (2 * aux2) + ObtenerFuncion(dato.ValorXB);
+            Area = (h / 3) * Area;
+
+            return Area;
+        }
+
+        //Simpson 3/8
+        public double MetodoSimpsonTresOctavos(Datos dato)
+        {
+            double Area = 0;
+            double h = (dato.ValorXB - dato.ValorXA) / 3;
+            double x1 = dato.ValorXA + h;
+            double x2 = x1 + h;
+            double x3 = x2 + h;
+
+            Area = ObtenerFuncion(dato.ValorXA) + (3 * ObtenerFuncion(x1)) + (3 * ObtenerFuncion(x2)) + ObtenerFuncion(x3);
+            Area = Area * ((3 / 8) * h);
+
+            return Area;
+        }
     }
 }
